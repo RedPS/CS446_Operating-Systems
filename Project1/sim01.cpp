1674ba745be3dbaef551bdbe3b68fbadd2d59884
Pedram <psafaei@nevada.unr.edu>
2019-02-19 12:13:49 -0800

    main driver, added comments

--- Project1/sim01.cpp
+++ Project1/sim01.cpp
@@ -1,6 +1,33 @@
+// Program Information //////////////////////
+/**
+ * @file sim01.cpp
+ * @brief The main driver for Project1
+ * @details Everything is done in other functions, this file is just a driver
+ * @version 1.0
+ * @author Pedram Safaei
+ * @bug nothing that can catched
+ * @note bugs have been catched according the instruction
+ */
 #include "Functions.h"
 
+// Program Description/Support ///////////////////////////////////
+/*
+    This program will accept a set of instructions by what this course 
+calls a metadata, it is basically a text file (mdf extention) with 
+some sets of instructions.
+    This program also uses a configuration file to set up Simulation 
+evnvironment for us. Which is again just a textfile (conf extention)
+
+                    Please run it by the following command
+
+                            ./sim01 config.conf
+*/
+// Main Function Implementation /////////////////////////////////// 
+//
 int main(int argc, char *argv[]){
+    //
+    // program code
+    //
     try{
         Configuration config;
         MetaData data;
@@ -11,7 +38,7 @@ int main(int argc, char *argv[]){
             config.LoadConfigurationFile(argv[1]);
             data.LoadMataData(config.Get_MetaDataFilePath(), MetaDatadata);
             for (auto& i : MetaDatadata){
-                Functions::ProcTime(config, i, status, pstatus);
+                Functions::ProcessTheTime(config, i, status, pstatus);
             }
             Functions::logtofile(config, MetaDatadata);
             return 0;
@@ -26,3 +53,4 @@ int main(int argc, char *argv[]){
         std::cerr << e.what() << std::endl;
     }
 }
+//
